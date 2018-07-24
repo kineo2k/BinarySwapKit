c04414a8b9562eeecd006c462507db61fff7612c
kineo2k <kineo2k@gmail.com>
2018-07-24 20:38:10 +0900

    Sample 코드 작성

--- src/Sample.ts
+++ src/Sample.ts
@@ -1,12 +1,11 @@
 import { BinaryBlockSwap } from './impl/BinaryBlockSwap';
 
 const swapOrder = [7, 3, 0, 4, 1, 2, 5, 6];
-const input = new Buffer('0123456789abcdef');
-let hrstart = process.hrtime();
-const bbs   = new BinaryBlockSwap(swapOrder);
-for (let i = 0; i < 10_000; i++) {
-	bbs.reverse(bbs.swap(input));
-}
-console.log(bbs.reverse(bbs.swap(input)).toLocaleString());
-let hrend = process.hrtime(hrstart);
-console.log(`Execution time (hr): `, hrend[0], hrend[1] / 1_000_000);
+const binaryInput = new Buffer('0123456789abcdef');
+
+const bbs = new BinaryBlockSwap(swapOrder);
+
+const swapped = bbs.swap(binaryInput);
+const reversed = bbs.reverse(swapped);
+
+console.log(`swapped  => ${swapped}\nreversed => ${reversed}`);
